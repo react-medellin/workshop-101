085060705d599732d8fb82de5431328822420807
Alejandro Nanez <alejonanez@gmail.com>
2017-08-06 21:38:36 -0500

    Exercise one

--- src/index.js
+++ src/index.js
@@ -5,18 +5,18 @@ import registerServiceWorker from './registerServiceWorker'; // For PWAs
 import 'bulma/css/bulma.css';
 import 'styles/app.css';
 
-function Hello() {
-    return (
-        <h1>Hello</h1>
-    );
-}
+/**
+ * Create a component called SeriesCard
+ * The wrapper element should be a <div>
+ * It should have an <img> with its SRC = http://static.tvmaze.com/uploads/images/medium_portrait/47/119795.jpg
+ * It should have an <h1> with the series name
+ * It should have a <p> with the series description
+ * It should have a <p> with the series status RUNNING / ENDED
+ *
+ * You have max 10 minutes for doing this
+ */
 
-// This is also an option. Same result as above
-// const Hello = () => <h1>Hello</h1>;
-
-// ReactDOM.render(What, Where);
-
-ReactDOM.render(<Hello />, document.getElementById('root'));
+// ReactDOM.render(Your component here..., document.getElementById('root'));
 
 // For PWAs
 registerServiceWorker();
